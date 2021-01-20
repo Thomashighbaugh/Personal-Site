90c5245a8455e8e22f1c13e8a1b04d44a06cc7ed
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2021-01-20 06:49:21 -0800

    fixing header issues

--- pages/index.js
+++ pages/index.js
@@ -8,16 +8,21 @@ const Index = () => {
   return (
     <Layout title="Index" className="index">
       <div className="introduction">
-        <div className="col-9">
+        <div>
           <h2 className="heading-2" id="intro">
             Hi! I&apos;m <br />
             <span className="introduction__content">Thomas Leon Highbaugh</span>
           </h2>
 
           <hr />
+          <div className="introduction__subtilte__container">
           <h3 className="heading-3 introduction__subtitle">
-            Web Developer from Hayward, California
+            Web Developer </h3>
+            <h3 className="heading-3 introduction__subtitle">Linux Systems Administrator</h3>
+              <h3 className="heading-3 introduction__subtitle">Graphics Designer
           </h3>
+          <h2 className="heading-6 introduction__location ">Monterey, California</h2>
+        </div>
         </div>
       </div>
     </Layout>
