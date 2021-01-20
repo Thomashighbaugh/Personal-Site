7112994f70711b940b8027867f89de044f7b10a2
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2021-01-20 07:23:40 -0800

    Committed by tlh

--- components/BlogRoll/BlogRoll.js
+++ components/BlogRoll/BlogRoll.js
@@ -4,7 +4,7 @@ import ReactMarkdown from "react-markdown";
 
 function BlogRoll({ allBlogs }) {
   function truncateSummary(content) {
-    return content.slice(0, 150) + "...";
+    return content.slice(0, 300) + "...";
   }
 
   function reformatDate(fullDate) {
