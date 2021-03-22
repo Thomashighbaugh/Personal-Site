d7dbbc16545374001aebd7bc4bdc5805a874ff39
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2021-03-22 04:26:39 -0700

    almost there, just some finishing touches

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -4,12 +4,12 @@ import SocialIcon from '@/components/SocialIcons'
 
 export default function Footer() {
   return (
-    <footer className="flex rounded-2xl m-0 flex-row align-center center justify-center py-2 items-center bg-white dark:bg-gray-900 border-4 border-gray-500 dark:border-gray-400">
-      <div className="  text-gray-500 dark:text-gray-400">
+    <footer className="flex flex-row items-center justify-center py-2 m-0 bg-white border-4 border-gray-500 rounded-2xl align-center center dark:bg-gray-900 dark:border-gray-400">
+      <div className="text-gray-500 dark:text-gray-400">
         <div className="relative inline-block transform group ">
           <Link
             href="/"
-            className="inline-block   font-sans font-black transform -rotate-1 md:rotate-0 group-hover:-rotate-6 border-0 transition-transform   uppercase  rounded-md shadow-md dark:hover:text-gray-50 hover:text-blue-500"
+            className="inline-block font-sans font-black uppercase transition-transform transform border-0 rounded-md shadow-md -rotate-1 md:rotate-0 group-hover:-rotate-6 dark:hover:text-gray-50 hover:text-blue-500"
           >
             {siteMetadata.author}
             {''}
@@ -19,11 +19,11 @@ export default function Footer() {
         </div>
       </div>
 
-      <div className="inline-flex  ml-6 space-x-4">
-        <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size="9" />
-        <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="9" />
-        <SocialIcon kind="twitter" href={siteMetadata.twitter} size="9" />
-        <SocialIcon kind="github" href={siteMetadata.github} size="9" />
+      <div className="inline-flex ml-6 space-x-4">
+        <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size="12" />
+        <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="12" />
+        <SocialIcon kind="twitter" href={siteMetadata.twitter} size="12" />
+        <SocialIcon kind="github" href={siteMetadata.github} size="12" />
       </div>
     </footer>
   )
