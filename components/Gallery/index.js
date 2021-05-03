a3490da4794ff464fcca34a83dcb7341807f434f
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2021-05-03 13:51:33 -0700

    the closest its been ever to done

--- components/Gallery/index.js
+++ components/Gallery/index.js
@@ -24,12 +24,12 @@ export default class Gallery extends Component {
   render() {
     const settings = {
       infinite: true,
-      speed: 900,
+      speed: 2220,
       slidesToShow: 1,
       slidesToScroll: 1,
       adaptiveHeight: true,
       autoplay: true,
-      autoplaySpeed: 700,
+      autoplaySpeed: 1900,
       centerMode: true,
       nextArrow: <SimpleNextArrow />,
       prevArrow: <SimplePrevArrow />,
@@ -50,31 +50,25 @@ export default class Gallery extends Component {
             alt="twitter card & banner"
           />{' '}
         </div>{' '}
-        <div>
+
+		<div>
           <img
             className="box-border p-0 m-0 bg-white border-2 border-gray-500 rounded-2xl dark:bg-gray-900 aspect-h-9 aspect-w-16"
-            src="/static/images/twitter-card-variant02.png"
+            src="/static/images/1.png"
             alt="twitter card & banner"
           />{' '}
         </div>{' '}
 		<div>
           <img
             className="box-border p-0 m-0 bg-white border-2 border-gray-500 rounded-2xl dark:bg-gray-900 aspect-h-9 aspect-w-16"
-            src="/static/images/01.png"
+            src="/static/images/3.png"
             alt="twitter card & banner"
           />{' '}
         </div>{' '}
 		<div>
           <img
             className="box-border p-0 m-0 bg-white border-2 border-gray-500 rounded-2xl dark:bg-gray-900 aspect-h-9 aspect-w-16"
-            src="/static/images/03.png"
-            alt="twitter card & banner"
-          />{' '}
-        </div>{' '}
-		<div>
-          <img
-            className="box-border p-0 m-0 bg-white border-2 border-gray-500 rounded-2xl dark:bg-gray-900 aspect-h-9 aspect-w-16"
-            src="/static/images/04.png"
+            src="/static/images/4.png"
             alt="twitter card & banner"
           />{' '}
         </div>{' '}
