7914ab17b4a1f61f293c87381065312e0f496573
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2021-05-03 23:54:41 -0700

    image fixes hopefully

--- components/Gallery/index.js
+++ components/Gallery/index.js
@@ -46,26 +46,25 @@ export default class Gallery extends Component {
         <div>
           <img
             className="box-border p-0 m-0 bg-white border-2 border-gray-500 rounded-2xl dark:bg-gray-900 aspect-h-9 aspect-w-16"
-            src="/static/images/twitter-card-variant01.png "
-            alt="twitter card & banner"
-          />{' '}
-        </div>{' '}
-
-		<div>
-          <img
-            className="box-border p-0 m-0 bg-white border-2 border-gray-500 rounded-2xl dark:bg-gray-900 aspect-h-9 aspect-w-16"
             src="/static/images/1.png"
             alt="twitter card & banner"
           />{' '}
         </div>{' '}
-		<div>
+        <div>
           <img
             className="box-border p-0 m-0 bg-white border-2 border-gray-500 rounded-2xl dark:bg-gray-900 aspect-h-9 aspect-w-16"
             src="/static/images/3.png"
             alt="twitter card & banner"
           />{' '}
         </div>{' '}
-		<div>
+        <div>
+          <img
+            className="box-border p-0 m-0 bg-white border-2 border-gray-500 rounded-2xl dark:bg-gray-900 aspect-h-9 aspect-w-16"
+            src="/static/images/twitter-card-variant01.png "
+            alt="twitter card & banner"
+          />{' '}
+        </div>{' '}
+        <div>
           <img
             className="box-border p-0 m-0 bg-white border-2 border-gray-500 rounded-2xl dark:bg-gray-900 aspect-h-9 aspect-w-16"
             src="/static/images/4.png"
