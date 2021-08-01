412e75a37fbec268d2952d285c1220a859359537
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2021-07-31 18:01:17 -0700

    trying alternative 1

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import { PageSeo } from '../components/SEO/index'
 import Image from 'next/image'
 import siteMetadata from '../data/siteMetadata'
 import Gallery from '../components/Gallery'
+import '../node_modules/slick-carousel/slick/slick.css'
+import '../node_modules/slick-carousel/slick/slick-theme.css'
 export default function Home() {
   return (
     <div>
