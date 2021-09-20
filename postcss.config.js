ebb9f4fe1f9e687939af63f609633fb0dfbd54bb
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2021-09-20 00:10:20 -0700

    hopefully this works

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-  plugins: ['tailwindcss', 'autoprefixer'],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production'
+      ? {
+          '@fullhuman/postcss-purgecss': {
+            // added sections folder and changed extension to jsx
+            content: [
+              './components/**/*.tsx',
+              './pages/**/*.tsx',
+              './content/**/**/*.ts',
+            ],
+            defaultExtractor: (content) =>
+              content.match(/[\w-/:]+(?<!:)/g) || [],
+          },
+        }
+      : {}),
+  },
 };
