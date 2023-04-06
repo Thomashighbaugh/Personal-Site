e3b7cc0d047fc9609e77bb13abad17a2517e7630
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2023-04-05 19:52:38 -0700

    Committed

--- src/components/Status/Widget/Loading.component.tsx
+++ src/components/Status/Widget/Loading.component.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import { LanyardAvatarType } from '~/types';
 
-interface LoadingProps {
-	type?: LanyardAvatarType;
-}
-
-const Loading: React.FC<LoadingProps> = ({ type }) => {
+const Loading: React.FC = () => {
 	return (
-		<div className="flex space-x-4 w-full max-w-sm mx-auto px-4 py-4 bg-white/50 dark:bg-primary-900/50 dark:text-primary-600 backdrop-filter backdrop-blur-sm bor-primary-bprimdrer-primary-200 rounded-lg hover:shadow-lg default-transition motion-safe:animate-pulse">
+		<div className="flex space-x-4 w-full max-w-sm mx-auto px-4 py-4 bg-white/50 dark:bg-primary-900/50 dark:text-primary-600 shadow-lg rounded-lg">
 			<div className="flex-1 space-y-4 py-1">
 				<div className="w-3/4 h-4 rounded bg-primary-rmary-200 d-primary-imary-imary-600" />
 				<div className="h-4 rounded bg-primary-ry-ry-200 d-primary-imary-imary-600" />
